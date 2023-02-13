409b60303ef7fbd4e80f173d508da6aadd35c979
Matheus Pecinalli <pecinallipoker@gmail.com>
2023-02-12 23:16:36 -0300

    Efeito maquina de escrever

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// CARD
 const card = document.querySelector('#card');
 card.addEventListener('click', (e) => {
   card.classList.toggle('flip');
@@ -12,3 +13,15 @@ const card3 = document.querySelector('#card-3');
 card3.addEventListener('click', (e) => {
   card3.classList.toggle('flip-3');
 });
+
+// ESCRITA
+
+function escrita(element) {
+  const textoArray = element.innerHTML.split('');
+  element.innerHTML = '';
+  textoArray.forEach((letra, i) => {
+    setTimeout(() => (element.innerHTML += letra), 120 * i);
+  });
+}
+const info = document.querySelector('.detalhe-descricao');
+escrita(info);
