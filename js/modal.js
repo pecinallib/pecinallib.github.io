08816b9a572b9b5674065fbc3964344f996e8f8b
Matheus Pecinalli <pecinallipoker@gmail.com>
2023-02-17 18:59:02 -0300

    Modal finalizado, e dando inicio a parte de contat

--- js/modal.js
+++ js/modal.js
@@ -1,1 +1,19 @@
-export default function initModal() {}
+export default function initModal() {
+  let imagens = document.querySelectorAll('.big-img');
+  let modal = document.querySelector('.container-modal');
+  let modalImg = document.querySelector('#modal-img');
+  let fechar = document.querySelector('#fechar');
+  let srcValue = '';
+
+  for (let i = 0; i < imagens.length; i++) {
+    imagens[i].addEventListener('click', function () {
+      srcValue = imagens[i].getAttribute('src');
+      modalImg.setAttribute('src', srcValue);
+      modal.classList.toggle('modal-ativo');
+    });
+  }
+
+  fechar.addEventListener('click', function () {
+    modal.classList.toggle('modal-ativo');
+  });
+}
